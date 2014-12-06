2382e6fc4f3fc75d48cf2a4b7a93bef2fa5560f9
nagstaku <nagstaku@gmail.com>
2014-12-06 15:58:37 -0800

    Revert "Revert "First Commit -- Blank MVC with basic Ping domain object and dummy pages""
    This reverts commit 60d1b67477b7c4a98e1594a2b89accf95c49a1b4.

---
+++ BikeFindr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BikeFindr.Startup))]
+namespace BikeFindr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
